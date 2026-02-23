9308b26d0e0d64aa8d0f0057791801c8cf473778
Tomato Suzuo <tz.cytpm@gmail.com>
2026-02-23 16:22:06 +0700

    Update default XFTP server list

--- src/Simplex/FileTransfer/Client/Presets.hs
+++ src/Simplex/FileTransfer/Client/Presets.hs
@@ -8,6 +8,10 @@ import Simplex.Messaging.Protocol (XFTPServerWithAuth)
 
 defaultXFTPServers :: NonEmpty XFTPServerWithAuth
 defaultXFTPServers =
-  [ "xftp://da1aH3nOT-9G8lV7bWamhxpDYdJ1xmW7j3JpGaDR5Ug=@xftp1.simplex.im,blisztokwh6alt5oem45f2eksm2xdowxszmhjesntcyolmhutoon7sid.onion",
-    "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp01.privateline.io"
+  [ "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp01.privateline.io",
+    "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp02.privateline.io",
+    "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp03.privateline.io",
+    "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp04.privateline.io",
+    "xftp://oJH7jdmfSr4C9fC8ONYIFNabnP_VatrHvjxq8nNXtMc=@xftp05.privateline.io",
+    "xftp://da1aH3nOT-9G8lV7bWamhxpDYdJ1xmW7j3JpGaDR5Ug=@xftp1.simplex.im,blisztokwh6alt5oem45f2eksm2xdowxszmhjesntcyolmhutoon7sid.onion"
   ]
